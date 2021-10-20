2383f7a9f5506de034527f0c178c32c2f67039bf
Aleksandr Morozov <dr.morozov@hotmail.com>
2021-10-20 22:57:06 +0300

    in progress

---
+++ Hacker News (SwiftUI)/Hacker News (SwiftUI)/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Hacker News (SwiftUI)
+//
+//  Created by Aleksandr Morozov on 20.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+        Text("Hello, world!")
+        Text("Good bye world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hacker News (SwiftUI)/Hacker News (SwiftUI)/Hacker_News__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News__SwiftUI_App.swift
+//  Hacker News (SwiftUI)
+//
+//  Created by Aleksandr Morozov on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
