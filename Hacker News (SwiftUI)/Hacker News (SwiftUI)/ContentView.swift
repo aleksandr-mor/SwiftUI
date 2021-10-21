30121983d15bf3f70d724d184fcd19e1fe72cea1
Aleksandr Morozov <dr.morozov@hotmail.com>
2021-10-21 23:36:43 +0300

    Add files via upload

--- Hacker News (SwiftUI)/Hacker News (SwiftUI)/ContentView.swift
+++ Hacker News (SwiftUI)/Hacker News (SwiftUI)/ContentView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
-        List {
-        Text("Hello, world!")
-        Text("Good bye world!")
+        NavigationView {
+            List(networkManager.posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("Hacker News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
         }
     }
 }
@@ -21,3 +29,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//let posts = [
+//Post(id: "1", title: "Hello"),
+//Post(id: "2", title: "Bonjour"),
+//Post(id: "3", title: "Hela")
+//]
---
+++ Hacker News (SwiftUI)/Hacker News (SwiftUI)/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  Hacker News (SwiftUI)
+//
+//  Created by Aleksandr Morozov on 21.10.2021.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
